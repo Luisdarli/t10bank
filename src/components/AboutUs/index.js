a17927e98fcb77aa91061bb5a5212de4f612ee1f
Luis Darli <luishenriquedarli@gmail.com>
2021-01-23 19:44:18 -0300

    add aboutus

---
+++ src/components/AboutUs/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css';
+import imagem from '../../assets/aboutus.png'
+import backgroundImage from '../../assets/background.png'
+
+export const AboutUs = () =>{
+    return(
+        <section className="app-aboutus">
+            <div className="app__aboutus-title">
+                <h1>Sobre nós</h1>
+            </div>
+
+            <div className="app__aboutus-wrapper">
+                <div className="app__aboutus__wrapper-tile">
+                    <img src={imagem} alt="Imagem sobre nós" className="app__aboutus-mainImage"></img>
+                    <img src={backgroundImage} alt="Imagem de fundo" className="app__aboutus-backgroundImage"></img>
+                </div>
+
+                <div className="app__aboutus__wrapper-tile">
+                    <h1 className="app__aboutus__wrapper-title">Qual seu próximo projeto?</h1>
+                    <p className="app__aboutus__wrapper-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat consequat mauris nunc congue nisi. Lorem ipsum dolor sit amet. Ut diam quam nulla porttitor massa id neque.</p>
+                    <span className="app__aboutus__wrapper-button">Download CV</span>
+                </div>
+            </div>
+
+        </section>
+    )
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Header } from '../Header'
 import { MainSection } from '../MainSection'
+import { AboutUs } from '../AboutUs'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainSection />
+      <AboutUs />
     </div>
   );
 }
