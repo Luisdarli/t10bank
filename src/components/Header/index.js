56666cdeed9f6d2f5c87beb24efa1033ae4b0b26
Luis Darli <luishenriquedarli@gmail.com>
2021-01-23 15:27:49 -0300

    init commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.css'
+import logo from '../../assets/logo.png'
+import { HeaderList } from './HeaderList'
+
+export const Header = () => {
+    return (
+        <header className="app__header">
+            <div className="app__header-img">
+                <img src={logo} alt="Logotipo T10 Bank"></img>
+            </div>
+
+            <div className="app__header-list">
+                <ul>
+                    <HeaderList item={"Home"} />
+                    <HeaderList item={"Sobre nós"} />
+                    <HeaderList item={"Serviços"} />
+                    <HeaderList item={"Portifólio"} />
+                    <HeaderList item={"Contato"} />
+                </ul>
+            </div>
+        </header>
+    )
+}
